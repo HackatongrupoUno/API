abc931433fe13e559bcc80ce3c0e25547fe84494
Daniela Lombana (CO) <daniela.lombana@bcdtravel.com.co>
2020-12-10 17:44:37 -0500

    Subo cambios danielaFuquene

--- db.js
+++ db.js
@@ -1,17 +1,13 @@
 const mongoose =require('mongoose');
-
-
 const config = require('./config')
 const conectDB = ()=>{
-   
     mongoose.connect('mongodb+srv://hackgrupo1:hackgrupo1@hackgroup1.evqtn.mongodb.net/HackGroup1?retryWrites=true&w=majority',{
         useNewUrlParser:true,useUnifiedTopology:true},(error)=>{
             if(error){
                 console.log('Error',error)
             }else{
-                console.log('Nos conectamos con exito a la DB de coodyExpert.')
+                console.log('We successfully connected to the coodyExpert DB.')
             }
         })
 }
-
 module.exports={conectDB} 
