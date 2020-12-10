e3284d050bb2969bbd0a8ae25a67f75b5f5bda82
vagar123 <vanessa.garnica.14@gmail.com>
2020-12-10 17:09:10 -0500

    Comentarios y/o codigo de ingles

--- controllers/notification.js
+++ controllers/notification.js
@@ -1,50 +1,35 @@
-const NotificationModel =require('../models/notification') //estamos requiriendo el modelo
-
-exports.create = (req, res) => {
-    //validamos que todos los datos del formulario esten llenos
+const NotificationModel = require('../models/notification') 
+exports.create = (req, res) => { 
     if (Object.entries(req.body).length == 0) {
-       return res.status(400).send({
-          message: 'los datos son obligatorios'
 
-      })
+        return res.status(400).send({
+            message: 'Los datos son obligatorios'
+        })
     }
-//vamos a crear una nueva postulacion
-    const notification = new NotificationModel({
+    const notification = new NotificationModel({ 
         statusNotification: req.body.statusNotification,
-        messageNotification:  req.body.messageNotification
+        messageNotification: req.body.messageNotification
     })
-    
-
-
-   
-    notification.save()// save metodo de mongoose
-        .then((datanotification) => { res.send(datanotification) })//si hace esto bien
-        .catch((error) => {//si no ejecute el cath
-            res.status(500) - send({//estado 500 (error de servidor)
-                message: error.message//este mensaje devolvera el error que mongoose tiene 
+    notification.save()
+        .then((datanotification) => { res.send(datanotification) })
+        .catch((error) => {
+            res.status(500) - send({
+                message: error.message
             })
         })
 }
-
-/**
- * Metodo para actualizar una empresa
- */
-
- exports.update=(req,res)=>{
-     //validacion de que todos los campos que se van a actualizar esten llenos 
+exports.update = (req, res) => {
     if (Object.entries(req.body).length == 0) {
-        return res.status(400).send({
-            message: 'los datos son obligatorios'
 
+        return res.status(400).send({
+            message: 'Los datos son obligatorios'
         })
     }
-
-    const notification= {
+    const notification = {
         statusNotification: req.body.statusNotification,
-        messageNotification:  req.body.messageNotification
- }
- //findByIdAndUpdate= metodo de mongoose que permite buscar por id y actualizar
- NotificationModel.findByIdAndUpdate(req.params.id, notification)
+        messageNotification: req.body.messageNotification
+    }
+    NotificationModel.findByIdAndUpdate(req.params.id, notification)
         .then(
             (notificationUpdate) => {
                 res.send(notificationUpdate)
@@ -57,5 +42,4 @@ exports.create = (req, res) => {
             }
         )
 }
-// aaaaa vale siii dale trankis 
-// jajajaj dale  desconectemosnos jejej buena noche vale chao
+
