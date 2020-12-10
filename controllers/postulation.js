d95d3828058181d9f977eae3ec598272ce8ac48e
dpsena <yeceniagmoreno2@gmail.com>
2020-12-10 17:31:25 -0500

    se modifican nombre de llaves

--- controllers/postulation.js
+++ controllers/postulation.js
@@ -1,41 +1,28 @@
-const PostulationModel =require('../models/postulation') //estamos requiriendo el modelo
-
+const PostulationModel =require('../models/postulation') 
 exports.create = (req, res) => {
-    //validamos que todos los datos del formulario esten llenos
     if (Object.entries(req.body).length == 0) {
+
        return res.status(400).send({
           message: 'los datos son obligatorios'
-
       })
     }
-//vamos a crear una nueva postulacion
     const postulation = new PostulationModel({
         titlePostulation: req.body.titlePostulation,
         description:  req.body.description,
-        cargo:  req.body. cargo,
+        position:  req.body.position,
         salary: req.body.salary,
-        languaje:req.body.languaje
+        language:req.body.language
     })
-    
-
-
-   
-    postulation.save()// save metodo de mongoose
-        .then((dataPostulation) => { res.send(dataPostulation) })//si hace esto bien
-        .catch((error) => {//si no ejecute el cath
-            res.status(500) - send({//estado 500 (error de servidor)
-                message: error.message//este mensaje devolvera el error que mongoose tiene 
+    postulation.save()
+        .then((dataPostulation) => { res.send(dataPostulation) })
+        .catch((error) => {
+            res.status(500) - send({
+                message: error.message
             })
         })
 }
-
-/**
- * Metodo para actualizar una empresa
- */
-
  exports.update=(req,res)=>{
-     //validacion de que todos los campos que se van a actualizar esten llenos 
-    if (Object.entries(req.body).length == 0) {
+       if (Object.entries(req.body).length == 0) {
         return res.status(400).send({
             message: 'los datos son obligatorios'
 
@@ -45,11 +32,10 @@ exports.create = (req, res) => {
     const postulation= {
         titlePostulation: req.body.titlePostulation,
         description:  req.body.description,
-        cargo:  req.body. cargo,
+        position:  req.body.position,
         salary: req.body.salary,
-        languaje:req.body.languaje
+        language:req.body.language
  }
- //findByIdAndUpdate= metodo de mongoose que permite buscar por id y actualizar
  PostulationModel.findByIdAndUpdate(req.params.id, postulation)
         .then(
             (postulationUpdate) => {
