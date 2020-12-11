5a519e97e813cbd7cf94359818ed46461123754d
nombreUsuario <cielangie2019@gmail.com>
2020-12-11 14:39:36 -0500

    quitendo la carpeta notificaciones

--- models/notification.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const notificationSchema = new mongoose.Schema({
-    statusNotification: {type: Boolean, required: true},
-    messageNotification: {type: String, required: true}, 
-})
-module.exports= mongoose.model('notification', notificationSchema)
